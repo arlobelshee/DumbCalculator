5e690efff0f5d6343bf8ca0c7df6cbc9d4bf3c4a
Arlo Belshee <arlo.belshee@live.com>
2018-05-10 00:04:47 +0200

    R#: introduce field.

--- DumbCalculator/Program.cs
+++ DumbCalculator/Program.cs
@@ -4,13 +4,7 @@ namespace DumbCalculator
 {
 	internal class Program
 	{
-		private static void Main(string[] args)
-		{
-			Console.WriteLine(
-				"I wish to do your bidding! I'm an RPN interpreter! I also support variables. Use the special command ? to get help.");
-			Console.Write("> ");
-			var input = Console.ReadLine();
-			if(input.Trim() == "?") Console.WriteLine(@"Supported commands:
+		private const string HelpInfo = @"Supported commands:
 	? -> print this help.
 	q -> quit.
 	[any decimal number] -> push that number onto the stack.
@@ -19,7 +13,18 @@ namespace DumbCalculator
 	* -> Pop the top 2 items on the stack, multiply them, push result onto the stack.
 	/ -> Pop the top 2 items on the stack, divide the top into the one under it, push result onto the stack.
 	=[name] -> pop the top of the stack and store it into a variable named `name`.
-	$[name] -> retrieve the value of the variable named `name` and push it onto the stack.");
+	$[name] -> retrieve the value of the variable named `name` and push it onto the stack.";
+
+		private static void Main(string[] args)
+		{
+			Console.WriteLine(
+				"I wish to do your bidding! I'm an RPN interpreter! I also support variables. Use the special command ? to get help.");
+			Console.Write("> ");
+			var input = Console.ReadLine();
+			if(input.Trim() == "?")
+			{
+				Console.WriteLine(HelpInfo);
+			}
 			Console.WriteLine("Quitting now.");
 			Console.ReadLine();
 		}
