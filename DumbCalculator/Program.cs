026447361cabc59d67ef97a72fe6e61c3ba61777
arlobelshee <arlo@belshee.com>
2020-04-29 18:13:30 -0600

    a   Autoformat the file

--- DumbCalculator/Program.cs
+++ DumbCalculator/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 
 namespace DumbCalculator
 {
-	internal class Program
-	{
-		private const string HelpInfo = @"Supported commands:
+    internal class Program
+    {
+        private const string HelpInfo = @"Supported commands:
 	? -> print this help.
 	q -> quit.
 	dump -> dump the contents of the stack and all variables.
@@ -20,131 +20,131 @@ namespace DumbCalculator
 	end formula -> stop defining a formula. Return state to what it was when defining started.
 	call [name] -> execute a formula.";
 
-		private static readonly Stack<decimal> Stack = new Stack<decimal>();
-		private static readonly Dictionary<string, decimal> Variables = new Dictionary<string, decimal>();
-		private static FormulaDefinition formulaBeingDefined = null;
-		private static readonly Dictionary<string, FormulaDefinition> formulas;
+        private static readonly Stack<decimal> Stack = new Stack<decimal>();
+        private static readonly Dictionary<string, decimal> Variables = new Dictionary<string, decimal>();
+        private static FormulaDefinition formulaBeingDefined = null;
+        private static readonly Dictionary<string, FormulaDefinition> formulas;
 
-		private static void Main(string[] args)
-		{
-			Console.WriteLine(
-				"I wish to do your bidding! I'm an RPN interpreter! I also support variables. Use the special command ? to get help.");
-			while (true)
-			{
-				Console.Write("> ");
-				var input = Console.ReadLine().Trim();
-				if (decimal.TryParse(input, out decimal number))
-				{
-					Stack.Push(number);
-				}
-				else if (input.StartsWith("def "))
-				{
-					formulaBeingDefined = new FormulaDefinition(input.Substring(4));
-				}
-				else if (input.Equals("end formula"))
-				{
-					if (null == formulaBeingDefined)
-					{ 
-						Console.WriteLine("No formula to end! Everything is unaltered.");
-					}
-					else
-					{
-						formulaBeingDefined.AddTo(formulas);
-						formulaBeingDefined = null;
-					}
-				}
-				else if (input.StartsWith("="))
-				{
-					if (Stack.Count == 0)
-					{
-						Console.WriteLine("Nothing to store! Variable unaltered.");
-					}
-					else
-					{
-						Variables[input.Substring(1)] = Stack.Pop();
-					}
-				}
-				else if (input.StartsWith("$"))
-				{
-					Stack.Push(Variables[input.Substring(1)]);
-				}
-				else
-				{
-					switch (input)
-					{
-						case "?":
-							Console.WriteLine(HelpInfo);
-							break;
-						case "+":
-							if (Stack.Count < 2)
-							{
-								Console.WriteLine("Not enough values to add! Please push more onto the stack and try again.");
-							}
-							else
-							{
-								var top = Stack.Pop();
-								var second = Stack.Pop();
-								Stack.Push(second + top);
-							}
-							break;
-						case "-":
-							if (Stack.Count < 2)
-							{
-								Console.WriteLine("Not enough values to subtract! Please push more onto the stack and try again.");
-							}
-							else
-							{
-								var top = Stack.Pop();
-								var second = Stack.Pop();
-								Stack.Push(second - top);
-							}
-							break;
-						case "*":
-							if (Stack.Count < 2)
-							{
-								Console.WriteLine("Not enough values to multiply! Please push more onto the stack and try again.");
-							}
-							else
-							{
-								var top = Stack.Pop();
-								var second = Stack.Pop();
-								Stack.Push(second * top);
-							}
-							break;
-						case "/":
-							if (Stack.Count < 2)
-							{
-								Console.WriteLine("Not enough values to divide! Please push more onto the stack and try again.");
-							}
-							else
-							{
-								var top = Stack.Pop();
-								var second = Stack.Pop();
-								Stack.Push(second / top);
-							}
-							break;
-						case "dump":
-							Console.WriteLine("Variables:");
-							foreach (var variable in Variables)
-							{
-								Console.WriteLine("	{0} := {1}", variable.Key, variable.Value);
-							}
-							Console.WriteLine("Stack");
-							foreach (var value in Stack)
-							{
-								Console.WriteLine("	{0}", value);
-							}
-							break;
-						case "q":
-							Console.WriteLine("Quitting now.");
-							Console.ReadLine();
-							return;
-						default:
-							Console.WriteLine("I have no idea what you mean. Use ? to ask for help if you want it.");
-							break;
-					}
-				}
-			}
-		}
-	}
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(
+                "I wish to do your bidding! I'm an RPN interpreter! I also support variables. Use the special command ? to get help.");
+            while (true)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine().Trim();
+                if (decimal.TryParse(input, out decimal number))
+                {
+                    Stack.Push(number);
+                }
+                else if (input.StartsWith("def "))
+                {
+                    formulaBeingDefined = new FormulaDefinition(input.Substring(4));
+                }
+                else if (input.Equals("end formula"))
+                {
+                    if (null == formulaBeingDefined)
+                    {
+                        Console.WriteLine("No formula to end! Everything is unaltered.");
+                    }
+                    else
+                    {
+                        formulaBeingDefined.AddTo(formulas);
+                        formulaBeingDefined = null;
+                    }
+                }
+                else if (input.StartsWith("="))
+                {
+                    if (Stack.Count == 0)
+                    {
+                        Console.WriteLine("Nothing to store! Variable unaltered.");
+                    }
+                    else
+                    {
+                        Variables[input.Substring(1)] = Stack.Pop();
+                    }
+                }
+                else if (input.StartsWith("$"))
+                {
+                    Stack.Push(Variables[input.Substring(1)]);
+                }
+                else
+                {
+                    switch (input)
+                    {
+                        case "?":
+                            Console.WriteLine(HelpInfo);
+                            break;
+                        case "+":
+                            if (Stack.Count < 2)
+                            {
+                                Console.WriteLine("Not enough values to add! Please push more onto the stack and try again.");
+                            }
+                            else
+                            {
+                                var top = Stack.Pop();
+                                var second = Stack.Pop();
+                                Stack.Push(second + top);
+                            }
+                            break;
+                        case "-":
+                            if (Stack.Count < 2)
+                            {
+                                Console.WriteLine("Not enough values to subtract! Please push more onto the stack and try again.");
+                            }
+                            else
+                            {
+                                var top = Stack.Pop();
+                                var second = Stack.Pop();
+                                Stack.Push(second - top);
+                            }
+                            break;
+                        case "*":
+                            if (Stack.Count < 2)
+                            {
+                                Console.WriteLine("Not enough values to multiply! Please push more onto the stack and try again.");
+                            }
+                            else
+                            {
+                                var top = Stack.Pop();
+                                var second = Stack.Pop();
+                                Stack.Push(second * top);
+                            }
+                            break;
+                        case "/":
+                            if (Stack.Count < 2)
+                            {
+                                Console.WriteLine("Not enough values to divide! Please push more onto the stack and try again.");
+                            }
+                            else
+                            {
+                                var top = Stack.Pop();
+                                var second = Stack.Pop();
+                                Stack.Push(second / top);
+                            }
+                            break;
+                        case "dump":
+                            Console.WriteLine("Variables:");
+                            foreach (var variable in Variables)
+                            {
+                                Console.WriteLine("	{0} := {1}", variable.Key, variable.Value);
+                            }
+                            Console.WriteLine("Stack");
+                            foreach (var value in Stack)
+                            {
+                                Console.WriteLine("	{0}", value);
+                            }
+                            break;
+                        case "q":
+                            Console.WriteLine("Quitting now.");
+                            Console.ReadLine();
+                            return;
+                        default:
+                            Console.WriteLine("I have no idea what you mean. Use ? to ask for help if you want it.");
+                            break;
+                    }
+                }
+            }
+        }
+    }
 }
