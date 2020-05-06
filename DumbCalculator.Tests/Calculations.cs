2a0341d58459f8e03fa7d6b62b726ce4d8d763f4
arlobelshee <arlo@belshee.com>
2020-05-06 15:57:11 -0600

    t   Verify that I can enter values onto the stack.

--- DumbCalculator.Tests/Calculations.cs
+++ DumbCalculator.Tests/Calculations.cs
@@ -8,6 +8,10 @@ namespace DumbCalculator.Tests
         [Fact]
         public void TakeInNumbers()
         {
+            Program.Stack.Clear();
+            Program.ProcessOneInput("-33.6");
+            Program.ProcessOneInput("2.5");
+            Program.Stack.Should().BeEquivalentTo(-33.6d, 2.5d);
         }
 	}
 }
