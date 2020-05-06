f656865b6c3e80d55bc60cb1cf3929081ede8ad1
arlobelshee <arlo@belshee.com>
2020-05-06 17:47:24 -0600

    F   Calculator now restores state upon completing function definition.

--- DumbCalculator/FormulaDefinition.cs
+++ DumbCalculator/FormulaDefinition.cs
@@ -1,12 +1,18 @@
-﻿namespace DumbCalculator
+﻿using System.Collections.Generic;
+
+namespace DumbCalculator
 {
     internal class FormulaDefinition
     {
-        public FormulaDefinition(string name)
+        public FormulaDefinition(string name, Stack<decimal> stack, Dictionary<string, decimal> variables)
         {
             Name = name;
+            InitialStack = stack;
+            InitialVariables = variables;
         }
 
         public string Name { get; set; }
+        public Stack<decimal> InitialStack { get; }
+        public Dictionary<string, decimal> InitialVariables { get; }
     }
 }
