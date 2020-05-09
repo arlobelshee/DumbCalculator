45d0af5b0e472d750dc0e699ad979fcd9d824e43
arlobelshee <arlo@belshee.com>
2020-05-08 19:51:03 -0600

    r   E-M 4

--- DumbCalculator.CppPort/DumbCalculator.CppPort.cpp
+++ DumbCalculator.CppPort/DumbCalculator.CppPort.cpp
@@ -49,7 +49,7 @@ int main()
 		std::string input;
 		std::getline(std::cin, input);
 
-		auto Applesauce = [&]() -> int {
+		auto Applesauce = [&input]() -> int {
 			double number;
 			if (TryParseDouble(input, number))
 			{
