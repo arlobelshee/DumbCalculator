260c8bfa743ddac286b103fd75e4af1e8870a3fd
arlobelshee <arlo@belshee.com>
2020-11-03 12:00:43 -0500

    t   Create a very simple test (just comments for now). Can't actually create the test, because the code is not testable!

--- DumbCalculator.Tests/Class1.cs
+++ DumbCalculator.Tests/Class1.cs
@@ -1,6 +1,17 @@
-﻿namespace DumbCalculator.Tests
+﻿using NUnit.Framework;
+
+namespace DumbCalculator.Tests
 {
+	[TestFixture]
 	public class Class1
 	{
+        [Test]
+        public void VerifyAddition()
+        {
+            // Push 2
+            // Push 4
+            // Push + operation
+            // Peek, verify stack is [6].
+        }
 	}
 }
