f0f0da87d3d5299ab647638912c1a9e8a216e31d
arlobelshee <arlo@belshee.com>
2020-11-03 18:03:24 -0500

    t   Stub in a test for addition, comments only.

---
+++ DumbCalculator.Test/CalculateBasicMath.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DumbCalculator.Test
+{
+    public class CalculateBasicMath
+    {
+        [Fact]
+        public void VerifyAddition()
+        {
+            // Push 2
+            // Push 4
+            // Push + operation
+            // Verify stack contains 6
+        }
+    }
+}
