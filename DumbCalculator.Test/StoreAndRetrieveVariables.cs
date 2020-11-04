688c17912e5d4732e958a5b1bd6c061c7251ad94
arlobelshee <arlo@belshee.com>
2020-11-04 11:37:27 -0500

    t   Verify current behavior for restore variable.

--- DumbCalculator.Test/StoreAndRetrieveVariables.cs
+++ DumbCalculator.Test/StoreAndRetrieveVariables.cs
@@ -13,5 +13,17 @@ namespace DumbCalculator.Test
             testSubject.HandleOneUserInput("=name");
             testSubject.StackContents.Should().BeEmpty();
         }
+
+        [Fact]
+        public void RestoreVariablePutsItOnTopOfTheStack()
+        {
+            var testSubject = new Calculator();
+            testSubject.HandleOneUserInput("3");
+            testSubject.HandleOneUserInput("=name");
+            testSubject.HandleOneUserInput("5");
+            testSubject.HandleOneUserInput("$name");
+            testSubject.HandleOneUserInput("$name");
+            testSubject.StackContents.Should().BeEquivalentTo(new decimal[] { 5, 3, 3 } );
+        }
     }
 }
